b99bef5053e1387786afa20b45fe1a84cbeff1be
Vivekkumar-12 <randhirkumar1411@gmail.com>
2025-11-21 13:21:12 +0530

    new 2

--- server/vite.ts
+++ server/vite.ts
@@ -45,12 +45,9 @@ export async function setupVite(app: Express, server: Server) {
     const url = req.originalUrl;
 
     try {
-      const clientTemplate = path.resolve(
-        import.meta.dirname,
-        "..",
-        "client",
-        "index.html",
-      );
+      
+    const clientTemplate = path.resolve(import.meta.dirname, "../dist/public/index.html");
+
 
       // always reload the index.html file from disk incase it changes
       let template = await fs.promises.readFile(clientTemplate, "utf-8");
@@ -69,7 +66,8 @@ export async function setupVite(app: Express, server: Server) {
 
 export function serveStatic(app: Express) {
   // Final Vite build output lives in dist/public
-  const distPath = path.resolve(import.meta.dirname, "../public");
+  const distPath = path.resolve(import.meta.dirname, "../dist/public");
+
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
